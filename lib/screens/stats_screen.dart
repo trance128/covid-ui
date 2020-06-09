ebfad8b2d0716ec10814f1d1865a044258365ee9
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-06-09 21:31:35 +0100

    Added stats_screen + supporting widgets

--- lib/screens/stats_screen.dart
+++ lib/screens/stats_screen.dart
@@ -1,4 +1,11 @@
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:flutter/material.dart';
+
+import '../config/palette.dart';
+import '../config/styles.dart';
+import '../data/data.dart';
+import '../widgets/custom_app_bar.dart';
+import '../widgets/widgets.dart';
 
 class StatsScreen extends StatefulWidget {
   @override
@@ -8,6 +15,101 @@ class StatsScreen extends StatefulWidget {
 class _StatsScreenState extends State<StatsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Palette.primaryColor,
+      appBar: CustomAppBar(),
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: [
+          _buildHeader(),
+          _buildRegionTabBar(),
+          _buildStatsTabBar(),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            sliver: SliverToBoxAdapter(
+              child: StatsGrid(),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(top: 20,),
+            sliver: SliverToBoxAdapter(
+              child: CovidBarChart(covidCases: covidUSADailyNewCases),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SliverPadding _buildHeader() {
+    return SliverPadding(
+      padding: const EdgeInsets.all(20),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          'Statistics',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildRegionTabBar() {
+    return SliverToBoxAdapter(
+      child: DefaultTabController(
+        length: 2,
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          height: 50,
+          decoration: BoxDecoration(
+            color: Colors.white24,
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: TabBar(
+            indicator: BubbleTabIndicator(
+              tabBarIndicatorSize: TabBarIndicatorSize.tab,
+              indicatorHeight: 40,
+              indicatorColor: Colors.white,
+            ),
+            labelStyle: Styles.tabTextStyle,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.white,
+            tabs: [
+              Text('My Country'),
+              Text('Global'),
+            ],
+            onTap: (index) {},
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverPadding _buildStatsTabBar() {
+    return SliverPadding(
+      padding: const EdgeInsets.all(20),
+      sliver: SliverToBoxAdapter(
+        child: DefaultTabController(
+          length: 3,
+          child: TabBar(
+            indicatorColor: Colors.transparent,
+            labelStyle: Styles.tabTextStyle,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white60,
+            tabs: [
+              Text('Total'),
+              Text('Today'),
+              Text('Yesterday'),
+            ],
+            onTap: (index) {},
+          ),
+        ),
+      ),
+    );
   }
 }
